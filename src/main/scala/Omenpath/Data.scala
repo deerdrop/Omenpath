ca6a411ab55f1f4ab1b0ffcb5229bf4bc0b82499
deerdrop <11528641+deerdrop@users.noreply.github.com>
2025-06-22 14:53:26 -0400

    season 8 prep

--- src/main/scala/Omenpath/Data.scala
+++ src/main/scala/Omenpath/Data.scala
@@ -1,9 +1,9 @@
 package Omenpath
 
 object Data:
-  val newSeason: Int = 7
+  val newSeason: Int = 8
   val minCardCount: Int = 1500
-  val minSetCount: Int = 7
+  val minSetCount: Int = 8
   val lockoutSeasons: Int = 2
   val scryfallFetchHeaders = Map("User-Agent" -> "OmenpathSetApp/0.1", "Accept" -> "*/*")
   lazy val ModernSets = Vector("TDM", "DFT", "FDN", "DSK", "BLB", "OTJ", "MKM", "LCI", "WOE", "MAT", "MOM", "ONE", "BRO", "DMU", "SNC", "NEO", "VOW", "MID", "AFR", "STX", "KHM", "ZNR", "M21", "IKO", "THB", "ELD", "M20", "WAR", "RNA", "GRN", "M19", "DOM", "RIX", "XLN", "HOU", "AKH", "AER", "KLD", "EMN", "SOI", "OGW", "BFZ", "ORI", "DTK", "FRF", "KTK", "M15", "JOU", "BNG", "THS", "M14", "DGM", "GTC", "RTR", "M13", "AVR", "DKA", "ISD", "M12", "NPH", "MBS", "SOM", "M11", "ROE", "WWK", "ZEN", "M10", "ARB", "CON", "ALA", "EVE", "SHM", "MOR", "LRW", "10E", "FUT", "PLC", "TSP", "CSP", "DIS", "GPT", "RAV", "9ED", "SOK", "BOK", "CHK", "5DN", "DST", "MRD", "8ED")
@@ -15,4 +15,5 @@ object Data:
     "Season 4" -> Vector("XLN", "SOI", "OGW", "SOM", "ALA", "TSP", "M10"),
     "Season 5" -> Vector("ELD", "VOW", "AFR", "CSP", "PLC", "AFR", "DGM", "AER"),
     "Season 6" -> Vector("NEO", "THB", "RNA", "DKA", "CON", "LRW", "BOK", "ORI"),
+    "Season 7" -> Vector("STX", "RAV", "WWK", "M14", "THS", "DIS", "BLB", "HOU")
   )
